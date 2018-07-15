7b2478727ebf4345ea86b69c49cba99f21e7cf44
weiwei wang <wilson.wang@ucloud.cn>
2018-07-15 21:12:53 +0800

    配置服务端渲染环境

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
-## 目录
+## 说明
+这是一个React项目的架构，主要理清项目目录的设置，构建工具的配置，服务端渲染的配置。
+
+## 启动
 ```shell
-build/  # webpack配置文件，工程脚本文件
-client/ # 应用文件
+npm run start # 启动项目
+```
+
+
+## 项目目录
+```shell
+build/              # webpack配置文件，工程脚本文件
+client/             # 应用文件
+dist/               # 编译后文件
+node_modules/       # npm包存储
+server/             # 存放后台服务文件
+  -- server.js      # 路由、服务、静态文件路径映射
+
 ```
---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const ReactSSR = require('react-dom/server');
+const serverEntry = require('../dist/server-entry').default;
+const fs = require('fs');
+const path = require('path');
+
+const tempalte = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8')
+
+const app = express()
+
+app.use('/public', express.static(path.join(__dirname, '../dist')))
+
+app.get('*', function (req, res) {
+  const appString = ReactSSR.renderToString(serverEntry)
+  let str = tempalte.replace('<app></app>', appString)
+  res.send(str)  
+})
+
+app.listen(3333, function() {
+  console.log('server is listening at http://localhost:3333');
+})
